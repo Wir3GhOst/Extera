2a3dd1bbc291b1960fa014ff097ec4ef19f40c2b
Muhammad Samaak <m.samaak00@gmail.com>
2018-02-09 20:35:40 +0400

    Add files via upload

---
+++ count_pdf_pages.py
@@ -0,0 +1,27 @@
+import sys,fnmatch,os,glob
+from PyPDF2 import PdfFileReader
+def count_pages():
+   input = sys.argv[1]
+   path = input+'/*.pdf'
+#def count_pages():
+   number =  len(fnmatch.filter(os.listdir(input), '*.pdf'))
+   print path   
+   total = 0
+   files=glob.glob(path)   
+   for file in files:     
+      f=open(file, 'r')
+      pdf_c = PdfFileReader(open(file,'rb'))
+      num1 = pdf_c.getNumPages()
+      total =  total + num1
+      print file +" ==> %d " % num1
+      f.readlines()   
+      f.close()
+   print "Number of PFD Files : %d" % number
+   print "Number of Pages is PFD Files : %d" % total
+
+
+if len(sys.argv) not in [2]:
+   print "Dir PATH ???"
+   exit()
+else: 
+    count_pages()
